72a6f887a9cd5debb81d6de2dd784509fa8a1446
Zuger Cédric <zuger.cedric@gmail.com>
2017-01-19 12:27:30 +0100

    Fix bootstrap css load issue.

--- lib/toronto.rb
+++ lib/toronto.rb
@@ -5,7 +5,7 @@ module Bootstrap
     class Engine < ::Rails::Engine
       config.app_generators.stylesheets false
 
-      initializer 'bootstrap-generators.setup', group: :all do |app|
+      initializer 'toronto.setup', group: :all do |app|
         app.config.less.paths << ::File.expand_path('../../vendor/twitter/bootstrap/less', __FILE__) if app.config.respond_to?(:less)
         app.config.assets.paths << ::File.expand_path('../../vendor/twitter/bootstrap/sass', __FILE__) if app.config.respond_to?(:sass)
 
