58f43b295973e729aa7024406eee73f8595bc27f
ced <zuger.cedric@gmail.com>
2017-08-26 11:38:40 +0200

    Fixing set locale.

--- lib/generators/toronto/install/templates/controllers/locales_controller.rb
+++ lib/generators/toronto/install/templates/controllers/locales_controller.rb
@@ -4,12 +4,22 @@ class LocalesController < ApplicationController
     # implement it this way : https://lingohub.com/frameworks-file-formats/rails5-i18n-ruby-on-rails/
     # Lead to a lot of problems
 
-    return_to = request.referer
+    if params[:locale]
+      I18n.locale = params[:locale]
+      return_to = request.referer
 
-    I18n.locale = params[:locale] if params[:locale]
-    # session[:locale] = params[:locale]
+      parsed_uri = URI.parse(return_to)
+      params = parsed_uri.query ? CGI::parse( parsed_uri.query ) : {}
+      params['locale'] = I18n.locale
 
-    redirect_to return_to + '?locale=' + I18n.locale.to_s
+      redirect_host = parsed_uri.scheme + '://' + parsed_uri.host
+      redirect_host += ( ':' + parsed_uri.port.to_s ) if parsed_uri.port && parsed_uri.port != 80 && parsed_uri.port != 443
+      redirect_host += parsed_uri.path
+
+      redirect_host += ( '?' + URI.encode_www_form(params) )
+
+      redirect_to redirect_host
+    end
   end
 
 end
